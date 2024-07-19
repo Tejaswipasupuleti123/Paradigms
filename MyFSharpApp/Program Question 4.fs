9d1027c04118aa9f86e276203baaf2cf348c0beb
Tejaswipasupuleti123 <tejpasupuleti123@gmail.com>
2024-07-19 15:30:03 -0400

    Update Program Question 4.fs

--- MyFSharpApp/Program Question 4.fs
+++ MyFSharpApp/Program Question 4.fs
@@ -1,8 +1,7 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-// Define the list of names
+
 let names = [" Charles"; "Babbage  "; "  Von Neumann  "; "  Dennis Ritchie  "]
 
-// Trim whitespace from each name and print
 printfn "Trimmed names:"
 names
 |> List.map (fun name -> name.Trim())
